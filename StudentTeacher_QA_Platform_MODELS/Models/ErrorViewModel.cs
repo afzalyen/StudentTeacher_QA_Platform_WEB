de257217acb0b8f0f1e8fb5972d8f2be1ee79595
afzalyen <afzal.hossain.yen@g.bracu.ac.bd>
2024-10-08 15:34:58 +0600

    Add project files.

---
+++ StudentTeacher_QA_Platform_MODELS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentTeacher_QA_Platform_MODELS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ StudentTeacher_QA_Platform_WEB/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentTeacher_QA_Platform_MODELS.Models;
+using System.Diagnostics;
+
+namespace StudentTeacher_QA_Platform_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
