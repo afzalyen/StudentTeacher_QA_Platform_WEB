6f49d733c9026d6563c352ccfbe6c1922907a12a
AfzalHossainYen <afzal.hossain.yen@g.bracu.ac.bd>
2024-10-09 02:33:17 +0600

    User Registration Login Logout Added

---
+++ StudentTeacher_QA_Platform_DATA_ACCESS/Identity/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentTeacher_QA_Platform_MODELS.Identity;
+
+namespace StudentTeacher_QA_Platform_DATA_ACCESS.Identity;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new ApplicationUserConfig());
+    }
+}
+
+public class ApplicationUserConfig : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Email).IsRequired().HasMaxLength(256);
+    }
+}
--- StudentTeacher_QA_Platform_WEB/Program.cs
+++ StudentTeacher_QA_Platform_WEB/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StudentTeacher_QA_Platform_WEB.Areas.Identity;
+using StudentTeacher_QA_Platform_DATA_ACCESS.Identity;
+using StudentTeacher_QA_Platform_MODELS.Identity;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -13,11 +25,12 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
